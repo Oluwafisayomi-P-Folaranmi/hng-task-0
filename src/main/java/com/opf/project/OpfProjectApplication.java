044a617ae8dc2f09d054cea8eff453769a05ca26
Oluwafisayomi-P-Folaranmi <folaranmifisayomi@gmail.com>
2025-02-04 22:25:06 +0100

    Initial commit

---
+++ src/main/java/com/opf/project/OpfProjectApplication.java
@@ -0,0 +1,13 @@
+package com.opf.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpfProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpfProjectApplication.class, args);
+	}
+
+}
